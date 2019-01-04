5db7c7a4232050fd8321548e501c4aee17f312e2
Anna Lisitska <foxik495@gmail.com>
2019-01-05 00:20:49 +0200

    some script

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,21 +1,37 @@
-var dialog = document.querySelector('dialog');
-document.querySelector('#show').onclick = function() {
-  dialog.show();
-};
-document.querySelector('#close').onclick = function() {
-  dialog.close();
-};
+$( document ).ready( function() {
+  let timer;
+  $( 'a[href="#prevItem"]' ).on( 'click', function() {
+    clearInterval(timer);
+    changeSlideMix( this, '.horizontal-slidermix', '.slide-itemmix' );
+    AutoPlay()
+  } );
+  $( 'a[href="#nextItem"]' ).on( 'click', function() {
+    clearInterval(timer);
+    changeSlideMix( this, '.horizontal-slidermix', '.slide-itemmix' );
+    AutoPlay()
+  } );
 
-function changeSlideMix(obj,block,li){
-  $(obj).parent().find(block+' '+li).stop(true,true);
-  if($(obj).hasClass('slider-prevmix')){
-  liLast=$(obj).parent().find(block+' '+li+':last');
-  liLast.prependTo(liLast.parent()).css('margin-left','-'+liLast.width()+'px').animate({marginLeft:0});}
-  else {
-  liFirst=$(obj).parent().find(block+' '+li+':eq(0)');
-  liFirst.animate({marginLeft:0-liFirst.width()},function(){
-  liFirst.css('margin-left',0).appendTo(liFirst.parent());
-  }); } }
-  $(document).ready(function(){
-  setInterval(function(){changeSlideMix('.slider-nextmix', '.horizontal-slidermix','.slide-itemmix')}, 2000);
-  });
+  function changeSlideMix( obj, block, li ) {
+    $( obj ).parent().find( block + ' ' + li ).stop( true, true );
+    if ( $( obj ).hasClass( 'slider-prevmix' ) ) {
+      const liLast = $( obj ).parent().find( block + ' ' + li + ':last' );
+      liLast.prependTo( liLast.parent() ).
+        css( 'margin-left', '-' + liLast.width() + 'px' ).
+        animate( { marginLeft: 0 } );
+    }
+    else {
+      const liFirst = $( obj ).parent().find( block + ' ' + li + ':eq(0)' );
+      liFirst.animate( { marginLeft: 0 - liFirst.width() }, function() {
+        liFirst.css( 'margin-left', 0 ).appendTo( liFirst.parent() );
+      } );
+    }
+  }
+
+  function AutoPlay() {
+    timer = setInterval( function() {
+      changeSlideMix( '.slider-nextmix', '.horizontal-slidermix',
+        '.slide-itemmix' );
+    }, 2000 );
+
+  }
+} );
